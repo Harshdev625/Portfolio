06a9a4305d86b76b88c2ab7f79852d69170f5c14
Harsh Dev <devh9933@gmail.com>
2023-08-04 14:53:24 +0530

    Message directed to Mail.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
   return (
     <>
       <div className="container">
@@ -12,7 +15,7 @@ const Contact = () => {
             <label htmlFor="name" className="form-label">
               <h5>Name</h5>
             </label>
-            <input type="text" className="form-control" id="name" />
+            <input type="text" className="form-control" id="name" onChange={(e)=>{setName(e.target.value)}}/>
           </div>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
@@ -23,16 +26,17 @@ const Contact = () => {
               className="form-control"
               id="email"
               aria-describedby="emailHelp"
+              onChange={(e)=>{setEmail(e.target.value)}}
             />
           </div>
           <div className="mb-3">
             <label htmlFor="message" className="form-label">
               <h5>Message</h5>
             </label>
-            <textarea className="form-control no-resize" id="message" rows="4" />
+            <textarea className="form-control no-resize" id="message" rows="4" onChange={(e)=>{setMessage(e.target.value)}}/>
           </div>
           <button type="submit" className="Send-Button grow">
-            <a className="Send-link" href="mailto:devh9933@gmail.com">Send</a>
+            <a className="Send-link" href={`mailto:devh9933@gmail.com?from=${email}&name=${name}&body=${message}`} >Send</a>
           </button>
         </form>
       </div>
